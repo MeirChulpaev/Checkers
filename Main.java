9502442cbfc3d0c005307ff1fd4cb8792665141d
meir <m050054052@gmail.com>
2024-06-11 16:50:21 +0300

    Return to original version

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
-public class Main03 {
+public class Main {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
         player[][] arr = {{new player(false, true, false, false), new player(false, false, true, false), new player(false, true, false, false), new player(false, false, true, false), new player(false, true, false, false), new player(false, false, true, false), new player(false, true, false, false), new player(false, false, true, false)},
                 {new player(false, false, true, false), new player(false, true, false, false), new player(false, false, true, false), new player(false, true, false, false), new player(false, false, true, false), new player(false, true, false, false), new player(false, false, true, false), new player(false, true, false, false)},
@@ -54,32 +55,24 @@ public class Main03 {
             int row1 = in.nextInt() - 1;
             System.out.println("and your column");
             int column1 = in.nextInt() - 1;
-            System.out.println("Enter the row you want to move");
+            System.out.println("Enter your row where you wont mov");
             int row2 = in.nextInt() - 1;
-            System.out.println("Enter the column you want to move");
+            System.out.println("and your column where you wont mov");
             int column2 = in.nextInt() - 1;
-            int a;
 
-            boolean IAmAKing = arr[row1][column1].isKing();
-            boolean TheRedPlayerDoesNotGoBack = (arr[row1][column1].isRed() && row1 < row2 || number == 0); //**
-            boolean TheBluePlayerDoesNotGoBack = (!arr[row1][column1].isRed() && row1 > row2 || number == 0);
 
-            if (IAmAKing || TheRedPlayerDoesNotGoBack || TheBluePlayerDoesNotGoBack) {
-                a = 0;
-                boolean ItsThatPlayersTurn=arr[row1][column1].getType().equals(p);
-                boolean HeIsKing=arr[row1][column1].getType().equals("♚") || arr[row1][column1].getType().equals("♔");
-                if (( ItsThatPlayersTurn || HeIsKing ) && CheckMOve(row1, column1, row2, column2, a, arr) && IfCanMove(row1, column1, arr, row2, column2) && number != 0) {
-                    DoTheMove(row1, column1, arr, row2, column2, a);
+            if (arr[row1][column1].isKing() || (arr[row1][column1].isRed() && row1 < row2) || (!arr[row1][column1].isRed() && row1 > row2 || number == 0)) {
+
+                if ((arr[row1][column1].getType().equals(p) || arr[row1][column1].getType().equals("♚") || arr[row1][column1].getType().equals("♔")) && CheckMOv1(row1, column1, row2, column2) && IfCanMov1(row1, column1, arr) && IfCanMov2(row2, column2, arr) && number != 0) {
+                    DoTheMov1(row1, column1, arr, row2, column2);
                     break;
                 }
-                a = 1;
-                if (( ItsThatPlayersTurn || HeIsKing ) && CheckMOve(row1, column1, row2, column2, a, arr) && IfCanMove(row1, column1, arr, row2, column2)) {
-                    boolean IfThePlaceIWantToMoveFromIsTheLastPlaceIWas= number == 0 && r2 != row1 && c2 != column1;
-                    if (IfThePlaceIWantToMoveFromIsTheLastPlaceIWas) {
+                if ((arr[row1][column1].getType().equals(p) || arr[row1][column1].getType().equals("♚") || arr[row1][column1].getType().equals("♔")) && CheckMOv2(row1, column1, row2, column2) && IfCanMov1(row1, column1, arr) && IfCanMov2(row2, column2, arr)) {
+                    if (number == 0 && r2 != row1 && c2 != column1) {
                         System.out.println("you cant eat");
                         break;
                     }
-                    DoTheMove(row1, column1, arr, row2, column2, a);
+                    DoTheMov2(row1, column1, arr, row2, column2);
                     System.out.println("If you want to eat more Enter 1 else Enter 0..");
                     number = in.nextInt();
                     if (number == 0)
@@ -90,86 +83,84 @@ public class Main03 {
                         c2 = column2;
                         number--;
                     }
+
                 }
             }
             if (number < 0)
                 System.out.println("please do again");
             else
                 System.out.println(" Enter next eat or fix your Mistake");
+
         }
+
     }
 
+    public static void DoTheMov1(int row, int column, player[][] arr, int row2, int column2) {
+        boolean t = arr[row][column].isKing();
 
-    public static boolean IfCanMove(int row, int column, player[][] arr, int row2, int column2) {
-        boolean MyPlaceIsNotEmpty = !arr[row][column].isSpeis();
-        boolean ThePlaceIWantToGoIsEmpty = arr[row2][column2].isSpeis();
-        boolean MyPlaceIsNotNull = !arr[row][column].isnull();
+        arr[row2][column2] = arr[row][column].copy();
+        if (row2 == 7 || row2 == 0)
+            arr[row2][column2].setKing(true);
+        if (t)
+            arr[row2][column2].setKing(true);
 
-        return (MyPlaceIsNotEmpty && MyPlaceIsNotNull && ThePlaceIWantToGoIsEmpty);
+        arr[row][column].setisspeis(true);
     }
 
-    public static boolean CheckMOve(int row1, int column1, int row2, int column2, int a, player[][] arr) {
+    public static boolean IfCanMov1(int row, int column, player[][] arr) {
+        return (!arr[row][column].isSpeis() || !arr[row][column].isnull());
 
-        boolean ThePlaceIWantToGetToIsACubeOrTwoAway_OptionA = (row1 == row2 + (a + 1) && column1 == column2 + (a + 1));
-
-        boolean ThePlaceIWantToGetToIsACubeOrTwoAway_OptionB = (row1 == row2 - (a + 1) && column1 == column2 - (a + 1));
-
-        boolean ThePlaceIWantToGetToIsACubeOrTwoAway_OptionC = (row1 == row2 - (a + 1) && column1 == column2 + (a + 1));
-        boolean ThePlaceIWantToGetToIsACubeOrTwoAway_OptionD = (row1 == row2 + (a + 1) && column1 == column2 - (a + 1));
-
-        if (a == 0)
-            return ThePlaceIWantToGetToIsACubeOrTwoAway_OptionA || ThePlaceIWantToGetToIsACubeOrTwoAway_OptionB ||
-                    ThePlaceIWantToGetToIsACubeOrTwoAway_OptionC || ThePlaceIWantToGetToIsACubeOrTwoAway_OptionD;
-        else
-            return (ThePlaceIWantToGetToIsACubeOrTwoAway_OptionA && !arr[row1 - 1][column1 - 1].isSpeis()) || (ThePlaceIWantToGetToIsACubeOrTwoAway_OptionB && !arr[row1 + 1][column1 + 1].isSpeis()) ||
-                    (ThePlaceIWantToGetToIsACubeOrTwoAway_OptionC && !arr[row1 + 1][column1 - 1].isSpeis()) || (ThePlaceIWantToGetToIsACubeOrTwoAway_OptionD && !arr[row1 - 1][column1 + 1].isSpeis());
     }
 
-    public static void DoTheMove(int row1, int column1, player[][] arr, int row2, int column2, int a) {
-        boolean IAmAKing = arr[row1][column1].isKing();
+    public static boolean IfCanMov2(int row2, int column2, player[][] arr) {
+        return arr[row2][column2].isSpeis();
+    }
+
+    public static boolean CheckMOv1(int row1, int column1, int row2, int column2) {
+        return (row1 == row2 + 1 && column1 == column2 + 1) || (row1 == row2 - 1 && column1 == column2 - 1) || (row1 == row2 - 1 && column1 == column2 + 1) || (row1 == row2 + 1 && column1 == column2 - 1);
+    }
+
+    public static boolean CheckMOv2(int row1, int column1, int row2, int column2) {
+        return (row1 == row2 + 2 && column1 == column2 + 2) || (row1 == row2 - 2 && column1 == column2 - 2) || (row1 == row2 - 2 && column1 == column2 + 2) || (row1 == row2 + 2 && column1 == column2 - 2);
+    }
+
+    public static void DoTheMov2(int row1, int column1, player[][] arr, int row2, int column2) {
+        boolean t = arr[row1][column1].isKing();
 
         arr[row2][column2] = arr[row1][column1].copy();
-
-        boolean PlayerReachedTheLastLine = (row2 == 7 || row2 == 0);
-
-        if (PlayerReachedTheLastLine)
+        if (row2 == 7 || row2 == 0)
             arr[row2][column2].setKing(true);
-        if (IAmAKing)
+        if (t)
             arr[row2][column2].setKing(true);
 
         arr[row1][column1].setisspeis(true);
-        if (a == 1) {
-            boolean OneOption0fDisplacement=row1 == row2 + 2 && column1 == column2 + 2;
-            boolean SecondOption0fDisplacement=row1 == row2 - 2 && column1 == column2 - 2;
-            boolean AThirdPossibilityOfDisplacement=row1 == row2 - 2 && column1 == column2 + 2;
-            boolean AFourthPossibilityOfDisplacement=row1 == row2 + 2 && column1 == column2 - 2;
-            if (OneOption0fDisplacement) {
-                arr[row1 - 1][column1 - 1].setisspeis(true);  //Delete the middle one
-            }
-            if (SecondOption0fDisplacement) {
-                arr[row1 + 1][column1 + 1].setisspeis(true);
-            }
-            if (AThirdPossibilityOfDisplacement) {
-                arr[row1 + 1][column1 - 1].setisspeis(true);
-            }
-            if (AFourthPossibilityOfDisplacement) {
-                arr[row1 - 1][column1 + 1].setisspeis(true);
-            }
+        if (row1 == row2 + 2 && column1 == column2 + 2) {
+            arr[row1 - 1][column1 - 1].setisspeis(true);
         }
+        if (row1 == row2 - 2 && column1 == column2 - 2) {
+            arr[row1 + 1][column1 + 1].setisspeis(true);
+        }
+        if (row1 == row2 - 2 && column1 == column2 + 2) {
+            arr[row1 + 1][column1 - 1].setisspeis(true);
+        }
+        if (row1 == row2 + 2 && column1 == column2 - 2) {
+            arr[row1 - 1][column1 + 1].setisspeis(true);
+        }
+
     }
 
     public static boolean Win(player[][] arr) {
-        int HowManyRedPlayersAreThereOnTheBoard = 0;
-        int HowManyBluePlayersAreThereOnTheBoard = 0;
+        int CountRed = 0;
+        int CountBlue = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
                 if (arr[i][j].isRed())
-                    HowManyRedPlayersAreThereOnTheBoard++;
+                    CountRed++;
                 if (!arr[i][j].isRed() && !arr[i][j].isKing() && !arr[i][j].isSpeis() && !arr[i][j].isnull())
-                    HowManyBluePlayersAreThereOnTheBoard++;
+                    CountBlue++;
             }
-
         }
-        return HowManyBluePlayersAreThereOnTheBoard == 0 || HowManyRedPlayersAreThereOnTheBoard == 0;
+        return CountRed == 0 || CountBlue == 0;
     }
+
 }
